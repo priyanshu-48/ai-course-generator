61aaaddbd054fd72132d488cce6044e7182bdb76
Priyanshu Singh <priyanshus204@gmail.com>
2025-10-22 23:33:08 +0530

    Make frontend public - remove login requirement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
 
 import Landing from './pages/Landing';
 import Login from './pages/Login';
@@ -16,50 +15,15 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<Dashboard />} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/add-course"
-            element={
-              <ProtectedRoute>
-                <AddCourse />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/course/:id"
-            element={
-              <ProtectedRoute>
-                <CoursePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/explore"
-            element={
-              <ProtectedRoute>
-                <Explore />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/upgrade"
-            element={
-              <ProtectedRoute>
-                <Upgrade />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/add-course" element={<AddCourse />} />
+          <Route path="/course/:id" element={<CoursePage />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/upgrade" element={<Upgrade />} />
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
@@ -69,7 +33,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
