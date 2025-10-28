94d919a36aca9d0cab054565f9edeb5f4cc6decf
Priyanshu Singh <priyanshus204@gmail.com>
2025-10-29 02:16:42 +0530

    Remove cleanup of demo courses

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,14 +1,6 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from courses.models import Course
-
-try:
-    Course.objects.delete()
-    print("All demo courses cleared on startup")
-except Exception as e:
-    print(f"Skipping demo cleanup: {e}")
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
